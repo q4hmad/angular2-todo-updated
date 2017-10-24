80f95435851b2b88a416766eea276da04459ee71
Dan Danilyuk and Margaret Berry <margaret@>
2017-10-24 11:52:01 -0700

    add edit/hide functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,26 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
-      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-      <h3>{{currentFocus}}</h3>
-      <ul>
+  <div class="container">
+    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+    <h3>{{currentFocus}}</h3>
+    <ul>
       <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}} <button (click)="editTask(currentTask)">Edit!</button></li>
-      </ul>
-      <hr>
-      <div>
+    </ul>
+    <hr>
+    <div>
+      <div *ngIf="selectedTask">
         <h3>{{selectedTask.description}}</h3>
         <p>Task Complete? {{selectedTask.done}}</p>
+        <hr>
         <h3>Edit Task</h3>
         <label>Enter Task Description:</label>
         <input [(ngModel)]="selectedTask.description">
-        <label>Enter Task Priority (1-3):</label>
-        <br>
+        <label>Enter Task Priority (1-3):</label><br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
         <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
+        <button (click)="finishedEditing()">Done</button>
       </div>
     </div>
+  </div>
     `
   })
 export class AppComponent {
@@ -36,9 +39,13 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
-  selectedTask: Task = this.tasks[0];
+  selectedTask = null;
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
+  }
+
+  finishedEditing() {
+    this.selectedTask = null;
   }
 
   isDone(clickedTask: Task) {
