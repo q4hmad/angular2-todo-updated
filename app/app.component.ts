7d75917dfcb4c7e98685641a326191e8a935fd76
Guest User <Guest@Epicodus-7A.local>
2017-10-24 09:04:22 -0700

    finish with buttons

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,17 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <div class="container">
-     <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-     <h3>{{currentFocus}}</h3>
-
-     <ul>
-       <li *ngFor="let currentTask of tasks">{{currentTask.description}}</li>
-     </ul>
-
-   </div>
-  `
-})
+      <div class="container">
+       <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+       <h3>{{currentFocus}}</h3>
+       <ul>
+         <li (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}<button (click)="editTask()">Edit!</button></li>
+       </ul>
+     </div>
+    `
+  })
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
@@ -25,6 +23,16 @@ export class AppComponent {
     new Task('Begin brainstorming possible JavaScript group projects'),
     new Task('Add README file to last few Angular repos on GitHub')
   ];
+  editTask() {
+   alert("You just requested to edit a Task!");
+  }
+  isDone(clickedTask: Task) {
+    if(clickedTask.done === true) {
+      alert("This task is done!");
+    } else {
+      alert("This task is not done. Better get to work!");
+    }
+  }
 }
 
 
