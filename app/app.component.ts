68ba0078d2e94077c81c3d323933e1d2a81e3c05
Margaret Berry and Qudsia Ahmad <qudsia@>
2017-10-25 11:49:21 -0700

    separate edit list component into separate file

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,29 +4,16 @@ import { Task } from './task.model';
 @Component({
   selector: 'app-root',
   template: `
-  <div class="container">
-    <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
-    <h3>{{currentFocus}}</h3>
-    <task-list></task-list>
-    <hr>
-    <div>
-      <div *ngIf="selectedTask">
-        <h3>{{selectedTask.description}}</h3>
-        <p>Task Complete? {{selectedTask.done}}</p>
-        <hr>
-        <h3>Edit Task</h3>
-        <label>Enter Task Description:</label>
-        <input [(ngModel)]="selectedTask.description">
-        <label>Enter Task Priority (1-3):</label><br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="1">1 (Low Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="2">2 (Medium Priority)<br>
-        <input type="radio" [(ngModel)]="selectedTask.priority" [value]="3">3 (High Priority)
-        <button (click)="finishedEditing()">Done</button>
-      </div>
+    <div class="container">
+      <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
+      <h3>{{currentFocus}}</h3>
+      <task-list [childTaskList]="masterTaskList" (clickSender)="editTask($event)"></task-list>
+      <hr>
+      <edit-task [childSelectedTask]="selectedTask" (doneButtonClickedSender)="finishedEditing()"></edit-task>
     </div>
-  </div>
     `
   })
+
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
@@ -35,6 +22,12 @@ export class AppComponent {
   year: number = this.currentTime.getFullYear();
 
   selectedTask = null;
+
+  masterTaskList: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
+  ];
 
   editTask(clickedTask) {
     this.selectedTask = clickedTask;
